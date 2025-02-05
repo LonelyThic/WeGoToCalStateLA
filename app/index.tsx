5e0461f129fa75426a96528f4090de45e5e7845e
Ryan <114775575+KiwiBirb95@users.noreply.github.com>
2025-02-05 00:11:06 -0800

    Rough landing page

---
+++ app/index.tsx
@@ -0,0 +1,85 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import Colors from "./../constant/Colors";
+import Corners from "./../constant/Corners";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        padding: 25,
+        height: "100%",
+        backgroundColor: Colors.SECONDARY,
+
+      }}
+    >
+      <Image source={require('./../assets/images/CSULA.png')} 
+      style={{
+        width: '100%',
+        height: '50%',
+        padding: 25,
+        alignSelf: 'center'}}
+      />
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: "50%",
+          borderTopLeftRadius: Corners.DEFAULT,
+          borderTopRightRadius: Corners.DEFAULT,
+          borderBottomLeftRadius: Corners.DEFAULT,
+          borderBottomRightRadius: Corners.DEFAULT,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: Colors.WHITE,
+          }}
+        >
+          WeGoToCalStateLA
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            textAlign: "center",
+            marginTop: 20,
+            color: Colors.WHITE,
+          }}
+        >
+          Welcome to the WeGoToCalStateLA application where you will be able to
+          learn new and creative ways to manage your money and improve your
+          well-being.
+        </Text>
+
+        <View style={styles.button}>
+          <Text style={[styles.buttonText, {color: Colors.WHITE}]}>Signup</Text>
+        </View>
+        <View style={[styles.button,{
+          backgroundColor: Colors.PRIMARY,
+          borderWidth: 1,
+          borderColor: Colors.WHITE,
+          }]}>
+          <Text style={[styles.buttonText,{color: Colors.WHITE}]}>Login</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 15,
+    backgroundColor: Colors.BLACK,
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 18,
+  }
+});
---
+++ constant/Colors.jsx
@@ -0,0 +1,6 @@
+export default{
+    WHITE: '#FFFFFF',
+    PRIMARY: '#1F4388',
+    SECONDARY: '#F6C344',
+    BLACK: '#000000',
+}
