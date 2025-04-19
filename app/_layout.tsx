dfba716130f4f6f8651eb0d790c81e0a6fb6f3d4
Ryan <114775575+KiwiBirb95@users.noreply.github.com>
2025-04-18 17:42:02 -0700

    Status bar adjustments

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,22 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native"; // NEW
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "./context/ThemeContext";
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          gestureEnabled: true,
-        }}
-      />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <>
+          <StatusBar backgroundColor="#003049" barStyle="light-content" />
+        </>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            gestureEnabled: true,
+          }}
+        />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
