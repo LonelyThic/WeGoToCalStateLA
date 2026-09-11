802f572fceeec3c37ab35de3fad6cd5f2e7d997d
LonelyThic <181782450+LonelyThic@users.noreply.github.com>
2026-09-11 13:03:31 -0700

    Update openIdClient.mjs

--- server/src/lib/openIdClient.mjs
+++ server/src/lib/openIdClient.mjs
@@ -1,4 +1,4 @@
-/*import * as client from 'openid-client'
+import * as client from 'openid-client'
 import {openIdConfig} from '../config/openIdConfig.mjs'
 const {google} = openIdConfig
 
@@ -9,19 +9,4 @@ export const openIdClient = {
         google.clientSecret
     ),
 
-}*/
-import * as client from 'openid-client'
-import {openIdConfig} from '../config/openIdConfig.mjs'
-const {google} = openIdConfig
-
-
-export const openIdClient = {
-    googleClient : google.clientId
-        ? await client.discovery(
-            google.issuer,
-            google.clientId,
-            google.clientSecret
-        )
-        : null,
 }
-
